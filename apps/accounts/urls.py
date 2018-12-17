9b72035ed2b609db05bc1936bb60929e8d882318
Kevin Dice <kevin.dice1@gmail.com>
2018-12-17 12:59:16 -0600

    Disable django rest-auth

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -14,61 +14,61 @@ from rest_auth.registration.views import (
 )
 
 
-class AgaveLogin(SocialLoginView):
-    adapter_class = AgaveAdapter
-
-
-class GlobusLogin(SocialLoginView):
-    adapter_class = GlobusAdapter
-
-
-class DropboxLogin(SocialLoginView):
-    adapter_class = DropboxOAuth2Adapter
-
-
-class JupyterHubLogin(SocialLoginView):
-    adapter_class = JupyterHubAdapter
-
-
-class AgaveConnect(SocialConnectView):
-    adapter_class = AgaveAdapter
-
-
-class GlobusConnect(SocialConnectView):
-    adapter_class = GlobusAdapter
-
-
-class DropboxConnect(SocialConnectView):
-    adapter_class = DropboxOAuth2Adapter
-
-
-class JupyterHubConnect(SocialConnectView):
-    adapter_class = JupyterHubAdapter
+# class AgaveLogin(SocialLoginView):
+#     adapter_class = AgaveAdapter
+#
+#
+# class GlobusLogin(SocialLoginView):
+#     adapter_class = GlobusAdapter
+#
+#
+# class DropboxLogin(SocialLoginView):
+#     adapter_class = DropboxOAuth2Adapter
+#
+#
+# class JupyterHubLogin(SocialLoginView):
+#     adapter_class = JupyterHubAdapter
+#
+#
+# class AgaveConnect(SocialConnectView):
+#     adapter_class = AgaveAdapter
+#
+#
+# class GlobusConnect(SocialConnectView):
+#     adapter_class = GlobusAdapter
+#
+#
+# class DropboxConnect(SocialConnectView):
+#     adapter_class = DropboxOAuth2Adapter
+#
+#
+# class JupyterHubConnect(SocialConnectView):
+#     adapter_class = JupyterHubAdapter
 
 
 urlpatterns = [
-    url(r'^', include('rest_auth.urls')),
-    url(r'^registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
-    url(r'^registration/', include('rest_auth.registration.urls')),
+    # url(r'^', include('rest_auth.urls')),
+    # url(r'^registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+    # url(r'^registration/', include('rest_auth.registration.urls')),
 
-    url(
-        r'^socialaccounts/$',
-        SocialAccountListView.as_view(),
-        name='social_account_list'
-    ),
-    url(
-        r'^socialaccounts/(?P<pk>\d+)/disconnect/$',
-        SocialAccountDisconnectView.as_view(),
-        name='social_account_disconnect'
-    ),
+    # url(
+    #     r'^socialaccounts/$',
+    #     SocialAccountListView.as_view(),
+    #     name='social_account_list'
+    # ),
+    # url(
+    #     r'^socialaccounts/(?P<pk>\d+)/disconnect/$',
+    #     SocialAccountDisconnectView.as_view(),
+    #     name='social_account_disconnect'
+    # ),
 
-    url(r'^agave/$', AgaveLogin.as_view(), name='agave_login2'),
-    url(r'^dropbox/$', DropboxLogin.as_view(), name='dropbox_login2'),
-    url(r'^globus/$', GlobusLogin.as_view(), name='globus_login2'),
-    url(r'^jupyterhub/$', JupyterHubLogin.as_view(), name='jupyterhub_login2'),
-
-    url(r'^agave/connect/$', AgaveConnect.as_view(), name='agave_connect'),
-    url(r'^dropbox/connect/$', DropboxConnect.as_view(), name='dropbox_connect'),
-    url(r'^globus/connect/$', GlobusConnect.as_view(), name='globus_connect'),
-    url(r'^jupyterhub/$', JupyterHubLogin.as_view(), name='jupyterhub_login2'),
+    # url(r'^agave/$', AgaveLogin.as_view(), name='agave_login2'),
+    # url(r'^dropbox/$', DropboxLogin.as_view(), name='dropbox_login2'),
+    # url(r'^globus/$', GlobusLogin.as_view(), name='globus_login2'),
+    # url(r'^jupyterhub/$', JupyterHubLogin.as_view(), name='jupyterhub_login2'),
+    #
+    # url(r'^agave/connect/$', AgaveConnect.as_view(), name='agave_connect'),
+    # url(r'^dropbox/connect/$', DropboxConnect.as_view(), name='dropbox_connect'),
+    # url(r'^globus/connect/$', GlobusConnect.as_view(), name='globus_connect'),
+    # url(r'^jupyterhub/$', JupyterHubLogin.as_view(), name='jupyterhub_login2'),
 ]
--- global/settings/base.py
+++ global/settings/base.py
@@ -72,8 +72,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_auth',
-    'rest_auth.registration',
+    //'rest_auth',
+    //'rest_auth.registration',
 
     'smuggler',
 
