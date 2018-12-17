fe3b283cb1fce111ebe8d5a7b9a54d340205a744
Kevin Dice <kevin.dice1@gmail.com>
2018-12-17 11:21:02 -0600

    Readd social login styles

--- global/settings/base.py
+++ global/settings/base.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/dj-static/'
+STATIC_URL = '/djstatic/'
 STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
